42f34957e3c52878823dff99ac57dfab96ac7392
jrient <jrient@qq.com>
2023-04-01 18:35:32 +0800

    v0.5

--- zsxq.py
+++ zsxq.py
@@ -5,10 +5,6 @@ import os
 group_id = os.getenv("ZSXQ_GROUP_ID")
 cookie = os.getenv("ZSXQ_COOKIE")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-group_id = '15552545558282'
-cookie = 'sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2248281551825888%22%2C%22first_id%22%3A%221870c143c6f10b3-0552622f9e3c428-26031951-2073600-1870c143c7011d2%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg3MGMxNDNjNmYxMGIzLTA1NTI2MjJmOWUzYzQyOC0yNjAzMTk1MS0yMDczNjAwLTE4NzBjMTQzYzcwMTFkMiIsIiRpZGVudGl0eV9sb2dpbl9pZCI6IjQ4MjgxNTUxODI1ODg4In0%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%2248281551825888%22%7D%2C%22%24device_id%22%3A%221870c143c6f10b3-0552622f9e3c428-26031951-2073600-1870c143c7011d2%22%7D; UM_distinctid=1870c33fe3f7e9-0ba29ad83f5afa-26031951-1fa400-1870c33fe4046c; zsxqsessionid=0261a538542d9b12990ae4dc485b0b3b; zsxq_access_token=DE1BF64E-CC81-DFC4-C555-631028EBF90F_445E47E846A1FE31; __cuid=434a40f4f7174764a09ceb194738df9b; amp_fef1e8=9e1a1bab-4e47-40cf-8231-66ae67f31201R...1gssadlrs.1gssafbqb.n.1.o; abtest_env=beta'
-openai.api_key = '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'
-
 if group_id is None:
     print("请设置环境变量 ZSXQ_GROUP_ID")
     exit()
